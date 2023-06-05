02407036e5de3e9d5bb7533127c179746afb9a81
huxley <elhuxley277@gmail.com>
2023-06-05 18:54:57 -0500

    last modifications of the project

--- test_main.py
+++ test_main.py
@@ -8,11 +8,8 @@ def test_home():
     response = client.get("/")
     assert response.status_code == 200
 
-<<<<<<< HEAD
 
-=======
 def test_route_product():
->>>>>>> heisenberg
     response = client.get("/product")
     assert response.status_code ==200
     
